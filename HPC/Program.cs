14e426103d19d0c17ef16be6b7e98cf3bc116ac5
BennyUCF <45030064+BennyUCF@users.noreply.github.com>
2025-07-11 10:26:26 -0400

    Add files via upload

---
+++ HPC/Program.cs
@@ -0,0 +1,195 @@
+﻿using RaPIDv2_HPC_1._0;
+using static RaPIDv2_HPC_1._0.Utl;
+
+internal class Program
+{
+    public static Utl.BufferWriterV3s BW;
+    public static Utl.G_Map gMap;
+    public static string VCF_Path;
+    public static string GMap_Path;
+    public static string Out_Path;
+    public static RandomProjection rdp;
+    public static Utl.VCF_Mem_v10rtr panel;
+
+    public static HPPBWT pal;
+
+
+    public static int currentPartition = 0;
+    public static int currentRDP = 0;
+    public static int nWorkerMatchUpdate = 10;
+    public static int rtrBlockSize = 500000000;
+    public static int msDA_Wait = 10;
+    public static int BW_BufferSize = 500;
+    public static int ms_L_Wait = 5;
+    public static int minSitePerWnd = 3;
+    public static int nSiteFixWnd = 3;
+    public static bool fixWnd_RDP = false;
+    public static int GC_Running = 0;
+
+    public static byte nRDP = 10;
+    public static int nOverlapTHD = 1;// -s 2 logic
+    public static double RDP_window_cM = 0.05;
+
+    public static double LLM_GLen = 5;
+    public static int nPartition = 2;
+    public static int firstPartition = 0;
+    public static int lastPartition = 1;
+
+    public static int nThread_PD;
+    public static int nThread_LM = 10;
+    public static int nThread_VO;
+    public static int nLM_Bucket = 100;
+    public static int nThread_Sort = 100;
+    public static int nThread_BitConv = -1;
+    public static int nThread_MAF = -1;
+
+    public static int nWriter = 1;
+    public static int nInstanceHP_PBWT = 10;
+    public static int nInstanceLM = 10;
+
+    static int phyCol = 3;
+    static int gRateCol = 2;
+    static char delim = ' ';
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("RaPID v2 HPC-1.0");
+        #region user inputs
+
+        if (args.Length != 10)
+        {
+            ShowManual();
+            return;
+        }
+
+        int nCore = Environment.ProcessorCount;
+        DateTime st = DateTime.Now;
+
+        VCF_Path = args[0];
+        GMap_Path = args[1];
+        LLM_GLen = Convert.ToInt32(args[2]);
+        Out_Path = args[3] + ".ibd";
+        nWriter = Convert.ToInt32(args[4]);
+
+
+        if (args[5].ToLower().StartsWith('f'))
+        {
+            fixWnd_RDP = true;
+            nSiteFixWnd = Convert.ToInt32(args[6]);
+        }
+        else
+        {
+            fixWnd_RDP = false;
+            RDP_window_cM = Convert.ToDouble(args[6]);
+        }
+
+        firstPartition = Convert.ToInt32(args[7]);
+        lastPartition = Convert.ToInt32(args[8]);
+        nPartition = Convert.ToInt32(args[9]);
+
+
+        Console.WriteLine("\nThe system has " + nCore + " logical cores.");
+
+        nThread_MAF = nCore / 6;
+        nThread_BitConv = nCore - 2 - nThread_MAF - 2;
+        nThread_PD = nCore / nInstanceHP_PBWT;
+        nThread_LM = nCore / nInstanceLM;
+        nThread_VO = nCore - nWriter - 2;
+        nThread_Sort = nCore - 2;
+        nLM_Bucket = nThread_Sort * 3;
+
+
+        Console.WriteLine("VCF_Path: " + VCF_Path);
+        Console.WriteLine("GMap_Path: " + GMap_Path);
+        Console.WriteLine("Genetic Length Cutoff: >=" + LLM_GLen);
+        Console.WriteLine("Out_Path: " + Out_Path);
+        Console.WriteLine("nWriter: " + nWriter);
+
+        if (fixWnd_RDP)
+        {
+            Console.WriteLine("RDP: F" + nSiteFixWnd);
+        }
+        else
+        {
+            Console.WriteLine("RDP: D" + RDP_window_cM.ToString("F4"));
+        }
+
+        Console.WriteLine("nThread_MAF: " + nThread_MAF);
+        Console.WriteLine("nThread_BitConv: " + nThread_BitConv);
+        Console.WriteLine("Partition: [" + firstPartition + "," + lastPartition + "]/" + nPartition);
+        Console.WriteLine("nLM_Bucket: " + nLM_Bucket);
+        Console.WriteLine("nInstanceLM: " + nInstanceLM);
+
+        Console.WriteLine();
+
+        #endregion
+
+        panel = new Utl.VCF_Mem_v10rtr(VCF_Path);
+
+
+        Task t1 = Task.Run(() => panel.Read());
+
+        panel.Set_L1_rPTR();
+
+        Task t2 = Task.Run(() => panel.LineSplitter());
+
+        Task t3 = Task.Run(() => panel.BitConverter());
+
+        Task t4 = Task.Run(() => panel.MafCalculator());
+
+        Task.WaitAll(t1, t2, t3, t4);
+
+        Program.gMap = new G_Map(Program.GMap_Path, phyCol, gRateCol, delim);
+
+
+        Program.gMap.Preload_Phy(panel.phyTags);
+        Program.rdp = new RandomProjection();
+        Program.gMap.Preload_Wnd();
+
+        pal = new HPPBWT();
+
+        Task t5 = Task.Run(() => pal.Run_PD());
+        t5.Wait();
+
+        HPPBWT.LM_VO_v3 lm = new HPPBWT.LM_VO_v3();
+        Task t6 = Task.Run(() => lm.Run());
+        if (lastPartition - firstPartition + 1 != nPartition)
+        {
+            Program.BW = new BufferWriterV3s(Program.Out_Path + ".part_"
+                + Program.firstPartition + "_" + Program.lastPartition + "_of_" + Program.nPartition);
+        }
+        else
+        {
+            Program.BW = new BufferWriterV3s(Program.Out_Path);
+        }
+
+        Task t7 = Task.Run(() => Program.BW.Run(false));
+
+        Task.WaitAll(t6, t7);
+
+        DateTime et = DateTime.Now;
+        TimeSpan span = et - st;
+        Console.WriteLine(DateTime.Now + " Done. " + span.TotalSeconds + " sec.");
+    }
+
+
+    public static void ShowManual()
+    {
+        Console.WriteLine("Required Inputs:");
+        Console.WriteLine("  [string]  VCF Path");
+        Console.WriteLine("  [string]  PLINK Genetic Map Path");
+        Console.WriteLine("  [decimal] IBD Length Threshold (in cM)");
+        Console.WriteLine("  [string]  IBD Output Path");
+        Console.WriteLine("  [int]     Number of Writers");
+        Console.WriteLine("  [char]    Random Projection Method: 'F' = Fixed window, 'D' = Dynamic window");
+        Console.WriteLine("  [int or decimal]    Windows Size: Use int Fixed window, Use decimal (in cM) for Dynamic window");
+        Console.WriteLine("  [int]     First Partition (inclusive)");
+        Console.WriteLine("  [int]     Last Partition (inclusive)");
+        Console.WriteLine("  [int]     Total Number of Partitions");
+        Console.WriteLine();
+        Console.WriteLine("Example:");
+        Console.WriteLine("RaPID_v2 my.vcf my.gmap 3.0 output.ibd 4 F 3 0 7 8");
+
+    }
+
+}
---
+++ HPC/RandomProjection.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaPIDv2_HPC_1._0
+{
+    public class RandomProjection
+    {
+        double RDP_window_cM = Program.RDP_window_cM;
+        int[][] selectedSites;//[nWnd,nRDP]
+        int[][] selectedSites_TRSP;//[nRDP,nWnd]
+        public List<int> windows = new List<int>();
+        public List<string> wndTags = new List<string>();
+
+        public int LM_StartWndID = -1;
+
+        public RandomProjection()
+        {
+            if (Program.fixWnd_RDP == false)
+            {
+                MakeWindows_Dynamic();
+            }
+            else
+            {
+                MakeWindows_Fix(Program.nSiteFixWnd);
+            }
+
+            SelectSites();
+
+
+        }
+
+        /// <summary>
+        /// returns a list of selected sites ID for a particular thread
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns>#item = # windows </returns>
+        public int[] Get_SelectedSites_ByThread(int t)
+        {
+            return selectedSites_TRSP[t];
+        }
+
+        /// <summary>
+        /// returns a list of selected site for a particular window
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns>#item = # thread</returns>
+        public int[] Get_SelectedSites_ByWindow(int w)
+        {
+            return selectedSites[w];
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="MAF_startIndex">inclusively</param>
+        /// <param name="MAF_endIndex">inclusively</param>
+        /// <returns></returns>
+        public List<int> SiteSelection_Weighted(int MAF_startIndex, int MAF_endIndex)
+        {
+            ConcurrentBag<int> res = new ConcurrentBag<int>();
+            double sum = 0;
+
+            for (int i = MAF_startIndex; i <= MAF_endIndex; i++)
+            {
+                sum += Program.panel.MAFs[i];
+            }
+
+            Parallel.For(0, Program.nRDP, (r) =>
+            {
+                Random rnd = new Random((int)DateTime.Now.Ticks + Thread.CurrentThread.ManagedThreadId);
+                double val = rnd.NextDouble() * sum;
+                double baseNum = 0;
+                for (int i = MAF_startIndex; i <= MAF_endIndex; i++)
+                {
+                    baseNum += Program.panel.MAFs[i];
+                    if (val <= baseNum)
+                    {
+                        res.Add(i);
+                        break;
+                    }
+                }
+            });
+
+            return res.ToList();
+        }
+
+        void MakeWindows_Dynamic()
+        {
+
+            double tgt = RDP_window_cM;
+
+            int i = 0;
+            int siteCnt = 0;
+            for (i = 0; i < Program.panel.nSite; i++)
+            {
+                siteCnt++;
+                if (Program.gMap.preLoaded_Rates[i] >= tgt && siteCnt >= Program.minSitePerWnd)
+                {
+                    windows.Add(i);
+                    wndTags.Add(Program.panel.phyTags[i]);
+                    tgt = Program.gMap.preLoaded_Rates[i] + RDP_window_cM;
+                    siteCnt = 0;
+                }
+
+            }
+
+            windows[windows.Count - 1] = i - 1;
+            wndTags[wndTags.Count - 1] = Program.panel.phyTags.Last();
+
+            double sum = 0;
+            for (i = 0; i < windows.Count; i++)
+            {
+
+                if (Program.gMap.preLoaded_Rates[windows[i]] >= Program.LLM_GLen)
+                {
+                    LM_StartWndID = i;
+                    break;
+                }
+            }
+         
+
+        }
+
+
+        void MakeWindows_Fix(int wndSize)
+        {
+
+            double tgt = RDP_window_cM;
+
+            int i = 0;
+
+            for (i = wndSize; i < Program.panel.nSite; i = i + wndSize)
+            {
+                windows.Add(i);
+                wndTags.Add(Program.panel.phyTags[i]);
+            }
+
+            windows[windows.Count - 1] = Program.panel.nSite - 1;
+            wndTags[wndTags.Count - 1] = Program.panel.phyTags.Last();
+
+
+            double sum = 0;
+            for (i = 0; i < windows.Count; i++)
+            {
+
+                if (Program.gMap.preLoaded_Rates[windows[i]] >= Program.LLM_GLen)
+                {
+                    LM_StartWndID = i;
+                    break;
+                }
+            }
+         
+
+        }
+
+        void SelectSites()
+        {
+            selectedSites = new int[windows.Count][];
+            selectedSites_TRSP = new int[Program.nRDP][];
+
+            for (int i = 0; i < windows.Count; i++)
+            {
+                selectedSites[i] = new int[Program.nRDP];
+            }
+
+            for (int i = 0; i < Program.nRDP; i++)
+            {
+                selectedSites_TRSP[i] = new int[windows.Count];
+            }
+
+            int s = 0;
+            List<int> selected;
+            for (int i = 0; i < windows.Count; i++)
+            {
+                selected = SiteSelection_Weighted(s, windows[i]);
+                for (int j = 0; j < selected.Count; j++)
+                {
+                    selectedSites[i][j] = selected[j];
+                    selectedSites_TRSP[j][i] = selected[j];
+                }
+                s = windows[i] + 1;
+            }
+        }
+    }
+}
